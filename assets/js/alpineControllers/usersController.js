b81b5df93df05cfbd8768fe4b99bb4446ceb3695
Mehrab <mehrab.par1314@gamil.com>
2026-09-06 18:05:12 -0700

    part #9

---
+++ assets/js/alpineControllers/usersController.js
@@ -0,0 +1,18 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('usersData', function(){
+        return {
+            users: [],
+            isLoading: false,
+            getUsers() {
+                this.isLoading =true
+                axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+                    this.users = res.data
+                }).catch(error =>{
+                    console.log(error.message);
+                }).finally(() => {
+                    this.isLoading = false
+                })
+            }
+        }
+    })
+})
